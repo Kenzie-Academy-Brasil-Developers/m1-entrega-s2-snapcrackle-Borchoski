a8e8c2daae58d2f3b65c74daf1a2c9d297881029
Joao Borchoski <joaohenriqueborchoski>
2022-08-18 15:04:19 -0300

    entrega

---
+++ index.js
@@ -0,0 +1,20 @@
+function snapCrackle(maxValue){
+    let lista = []
+    for(let i = 1; i <= maxValue; i++){
+        if(i % 2 == 1 && i % 5 == 0){
+            lista.push("Snap Crackle")
+        }
+        else if(i % 5 == 0){
+            lista.push("Crackle")
+        }
+        else if(i % 2 == 1){
+            lista.push("Snap")
+        }else{
+            lista.push(i)
+        }
+    }
+    let lista2 = lista.join(', ')
+    return lista2
+}
+//console.log(snapCrackle(10))
+
